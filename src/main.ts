1ccb0e357522802ef30eb573914b7dba3043828c
Mohammed Aashik <aashikdevelop@gmail.com>
2026-05-07 02:05:57 +0530

    chore(config): add environment configuration &&

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 4000;
+
+  await app.listen(port);
+
+  console.log(`Server running on http://localhost:${port}`);
 }
+
 bootstrap();
