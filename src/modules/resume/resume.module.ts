af8816c9a153f802a11f059d3dffa72d0de5e44e
Mohammed Aashik <aashikdevelop@gmail.com>
2026-05-07 02:53:25 +0530

    feat: import AiModule and DbModule into ResumeModule

--- src/modules/resume/resume.module.ts
+++ src/modules/resume/resume.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { DbModule } from '../../db/db.module';
+import { AiModule } from '../ai/ai.module';
 import { ResumeController } from './resume.controller';
 import { ResumeService } from './resume.service';
 
 @Module({
+  imports: [AiModule, DbModule],
   controllers: [ResumeController],
-  providers: [ResumeService]
+  providers: [ResumeService],
 })
 export class ResumeModule {}
